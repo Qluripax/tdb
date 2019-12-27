7f3ad78f3e8d8312ca532ade6fd75abb62576afe
Magnus <magnus@docilitas.se>
2019-12-27 13:43:16 +0100

    1:st version with all timetables

--- tdb.js
+++ tdb.js
@@ -1,43 +1,47 @@
 // set year to be handled
 let year = 2020;
 // Set first and last week to be printed within the calendar
-let weeksToShow = [24, 37];
+let weeksToShow = [23, 37];
 
 let trafficDays = [
-    {type: 'steam', legend: 'Dag med Ångloks- och rälsbusstrafik',
+    {
+        type: 'steam', legend: 'Dag med Ångloks- och rälsbusstrafik',
+        caption: 'Lördagar 27 juni - 22 aug, onsdagar i Juli',
         dates: ['2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-08', '2020-07-15', '2020-07-22',
             '2020-07-29', '2020-06-27', '2020-07-11', '2020-07-18', '2020-07-25', '2020-08-01', '2020-08-01', '2020-08-08',
-            '2020-08-15', '2020-08-22']},
-    {type: 'railbus', legend: 'Dag med Rälsbusstrafik',
-        dates: ['2020-07-10', '2020-07-17', '2020-07-24', '2020-07-31']},
-    {type: 'special', legend: "Dag med specialtidtabell, se&nbsp;www.nbvj.se",
-        dates: ['2020-06-13', '2020-06-19', '2020-08-29', '2020-08-30']},
-];
-
-let trains = [
-    {type: 'railbus', nr: 2, name: 'Kaffetåget', tdb: [{sign: 'N', time: '10:30'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
-            {sign: 'Lm', time: 'x'}, {sign: 'J', time: '10:50'}]},
+            '2020-08-15', '2020-08-22']
+    },
+    {
+        type: 'railbus', legend: 'Dag med Rälsbusstrafik',
+        caption: 'Fredagar 10, 17, 24 & 31 juni',
+        dates: ['2020-07-10', '2020-07-17', '2020-07-24', '2020-07-31']
+    },
+    {
+        type: 'special', legend: "Dag med specialtidtabell, se&nbsp;www.nbvj.se",
+        caption: '',
+        dates: ['2020-06-13', '2020-06-19', '2020-08-29', '2020-08-30']
+    },
 ];
 
 let trafficPlaces = [
-    {km: '17,6', sign: 'N', name: 'Fr Nora', class: 'font-weight-bold'},
+    {km: '17,6', sign: 'fr N', name: 'Fr Nora', class: 'font-weight-bold'},
     {km: '14,0', sign: 'Kh', name: 'Källarhalsen', class: 'float-right'},
     {km: '12,6', sign: 'Sm', name: 'Stora Mon', class: 'float-right'},
     {km: '10,2', sign: 'lm', name: 'Lilla Mon', class: 'float-right'},
-    {km: '8,2', sign: 'J', name: 't Järle', class: 'font-weight-bold border-bottom'},
-    {km: '8,2', sign: 'J', name: 'fr Järle', class: 'font-weight-bold'},
-    {km: '8,2', sign: 'Tp', name: 'Torpa', class: 'float-right'},
-    {km: '8,2', sign: 'Tp', name: 't Löth', class: 'font-weight-bold border-bottom'},
+    {km: '8,2', sign: 't J', name: 't Järle', class: 'font-weight-bold border-bottom'},
+    {km: '8,2', sign: 'fr J', name: 'fr Järle', class: 'font-weight-bold'},
+    {km: '6,4', sign: 'Tp', name: 'Torpa', class: 'float-right'},
+    {km: '2,8', sign: 't Lt', name: 't Löth', class: 'font-weight-bold border-bottom'},
     {km: '19,5', sign: 'Phn', name: 'Pershyttevägen', class: 'float-right'},
     {km: '20,9', sign: 'Ph', name: 'Pershyttan', class: 'float-right'},
-    {km: '22,9', sign: 'Phö', name: 't Pershyttan Övre', class: 'font-weight-bold'},
-    {km: '22,9', sign: 'Phö', name: 'fr Pershyttan Övre', class: 'font-weight-bold'},
+    {km: '22,9', sign: 't Phö', name: 't Pershyttan Övre', class: 'font-weight-bold'},
+    {km: '22,9', sign: 'fr Phö', name: 'fr Pershyttan Övre', class: 'font-weight-bold'},
     {km: '22,6', sign: 'Gt', name: 't Gyttorp', class: 'font-weight-bold'},
-    {km: '23,5', sign: 'Kt', name: 'Käppsta (badtåg)', class: 'float-right'},
+    {km: '23,5', sign: 'Kä', name: 'Käppsta (badtåg)', class: 'float-right'},
     {km: '25,8', sign: 'Kp', name: 'Knapptorp', class: 'float-right'},
     {km: '28,0', sign: 'Bt', name: 'Bengtstorp', class: 'float-right'},
     {km: '30,7', sign: 'Vk', name: 'Vikersvik', class: 'float-right'},
-    {km: '33,7', sign: 'Nvk', name: 'Vikerskolorna', class: 'font-weight-bold'},
+    {km: '32,6', sign: 'Nvk', name: 'Vikerskolorna', class: 'font-weight-bold'},
 ];
 
 function initPage() {
@@ -45,53 +49,115 @@ function initPage() {
     initCalendar();
     setTrafficDays();
     viewLegend();
-    initTimeTable('tdb-1');
+    initTimeTable('tdb-1', 'railbus');
+    initTimeTable('tdb-2', 'steam');
+    initTimeTable('tdb-3', 'railbus', true);
+    initTimeTable('tdb-4', 'steam', true);
 }
 
-function initTimeTable(tdb) {
+function initTimeTable(tdb, dayType, direction = false) {
 
-    let timeTable = '<table class="tdb"><thead><tr><th colspan="2">Fredagar</th></tr></thead>';
-    timeTable += '<tbody><tr><td></td><td>&nbsp;</td><td class="rotate">Kaffetåget</td></tr>';
-    timeTable += '<tr><td></td><td class="small float-right">tågnummer:</td><td>2</td></tr>';
-
-    timeTable += '<tr><td class="small pre">Km</td><td class="small">Stationer/hållplatser</td></tr>';
-    $.each(trafficPlaces, function (i, trafficPlace) {
-        let border = (trafficPlace.class.indexOf('border-bottom') >= 0) ? 'border-bottom' :'';
-
-        timeTable += '<tr><td class="small right ' + border +'">' + trafficPlace.km +
-            '</td><td class="small ' + trafficPlace.class +'">' + trafficPlace.name + '</td></tr>';
+    let caption = '';
+    let _trains = [];
+    $.each(trafficDays, function (i, day) {
+        if (day.type === dayType) {
+            caption = day.caption;
+        }
     });
+    $.each(trains, function (i, train) {
+        if (train.days === dayType && train.reversed === direction) {
+            _trains.push(train);
+        }
+    });
+    let timeTable = '<table class="tdb"><thead><tr><th class="center border-bottom" colspan="' + (_trains.length + 2) + '">' +
+        caption + '</th></tr></thead><tbody><tr class="trainName"><td></td><td>&nbsp;</td>';
+
+    $.each(_trains, function (i, train) {
+        timeTable += '<td class="rotate train">' + train.name + '</td>';
+    });
+    timeTable += '</tr><tr><td></td><td class="small float-right">tågnummer:</td>';
+    $.each(_trains, function (i, train) {
+        timeTable += '<td class="center">' + train.nr + '</td>';
+    });
+//    timeTable += '</tr><tr><td></td><td></td>';
+    timeTable += '</tr><tr><td class="small pre km border-bottom">Km</td><td class="small border-bottom">' +
+        'Stationer/hållplatser</td>';
+
+    $.each(_trains, function (i, train) {
+        timeTable += '<td class="center"><img src="assets/' + train.type + '.png" style="max-width:50px;max-height:18px;"></td>';
+    });
+
+//    timeTable += '</tr><tr><td class="small pre km border-bottom">Km</td><td class="small border-bottom">' +
+//        'Stationer/hållplatser</td><td class="border-bottom" colspan="' + _trains.length + '" </tr>';
+
+    // loop through each place
+    if (!direction) {
+        $.each(trafficPlaces, function (i, trafficPlace) {
+            let border = (trafficPlace.class.indexOf('border-bottom') >= 0) ? 'border-bottom' : '';
+
+            timeTable += '<tr><td class="small right km ' + border + '">' + trafficPlace.km +
+                '</td><td class="small ' + trafficPlace.class + '">' + trafficPlace.name + '</td>';
+            timeTable += _getTimeforPlace(_trains, trafficPlace.sign, border + ' ' + dayType) + '</tr>';
+        });
+    } else {
+        let i = trafficPlaces.length;
+        while (i--) {
+            let trafficPlace = trafficPlaces[i];
+            let border = (trafficPlace.class.indexOf('border-bottom') >= 0) ? 'border-bottom' : '';
+            let place = trafficPlace.name.replace('fr ', 'from ').replace('t ', 'to ');
+            timeTable += '<tr><td class="small right km ' + border + '">' + trafficPlace.km +
+                '</td><td class="small ' + trafficPlace.class + '">' + place.replace('from ', 't ')
+                    .replace('to ', 'fr ').replace('Fr ', 'T ') + '</td>';
+            timeTable += _getTimeforPlace(_trains, trafficPlace.sign, border + ' ' + dayType) + '</tr>';
+        }
+
+    }
     timeTable += '</tbody></table>';
-    $('#tdb-1').html(timeTable);
+    $('#' + tdb).html(timeTable);
+}
+
+function _getTimeforPlace(trains, place, classes) {
+    let placeInfo = '';
+    $.each(trains, function (i, train) {
+        let trainInfo = '<td class="center ' + classes + '"></td>';
+        $.each(train.tdb, function (ix, stop) {
+            if (stop.sign === place) {
+                trainInfo = '<td class="small center ' + classes + '">' + stop.time + '</td>';
+            }
+        });
+        placeInfo += trainInfo;
+    });
+    return placeInfo;
 }
 
 function setTrafficDays() {
     $.each(trafficDays, function (i, trafficDay) {
         $.each(trafficDay.dates, function (ix, date) {
             let tDate = moment(date).dayOfYear();
-            $('td#'+tDate).addClass(trafficDay.type);
+            $('td#' + tDate).addClass(trafficDay.type);
         })
     });
 }
 
 function viewLegend() {
-    let legend = '<table class="legend "><tbody><tr><td class="date border">1</td><td class="text">Dag utan trafik</td></tr>';
+    let legend = '<table class="legend "><tbody><tr><td class="pre date border">1</td><td class="pre text">Dag utan trafik</td></tr>';
     $.each(trafficDays, function (i, trafficDay) {
-        legend += '<tr><td class="date ' + trafficDay.type + '">' + moment(trafficDay.dates[0]).format('D') +
-            '</td><td class="text">' + trafficDay.legend + '</td></tr>'
+        legend += '<tr><td class="pre date ' + trafficDay.type + '">' + moment(trafficDay.dates[0]).format('D') +
+            '</td><td class="pre text">' + trafficDay.legend + '</td></tr>'
     });
 
     legend += '</tbody></table>';
     $('#legend').html(legend);
 
 }
+
 function initCalendar() {
     let calendar = '<table class="cal"><thead><tbody><tr><th class="weekday"></th>';
 
     let firstDate = moment().year(year).week(weeksToShow[0]).day('monday');
     let lastDate = moment().year(year).week(weeksToShow[1]).day('sunday');
     let numberOfWeeks = (weeksToShow[1] - weeksToShow[0]);
-        let allMonthsInPeriod = [];
+    let allMonthsInPeriod = [];
     let colspansInPeriod = [];
 
     let startDate = moment(firstDate.format("YYYY-MM-01"));
@@ -107,12 +173,12 @@ function initCalendar() {
             let firstDay = moment(startDate).format("D");
             let lastDay = moment(lastDate).format('D');
             colSpan = Math.floor((lastDate.day() + lastDay) / 7);
-            colSpan = (colSpan >= numberOfWeeks) ? numberOfWeeks: colSpan;
+            colSpan = (colSpan >= numberOfWeeks) ? numberOfWeeks : colSpan;
         } else {
             // set all other months
             colSpan = Math.round((startDate.day() + moment(startDate).daysInMonth()) / 7);
         }
-        numberOfWeeks = numberOfWeeks- colSpan;
+        numberOfWeeks = numberOfWeeks - colSpan;
         colspansInPeriod.push(colSpan);
         allMonthsInPeriod.push(startDate.format("YYYY-MM"));
 
@@ -142,7 +208,7 @@ function initCalendar() {
     let dayNumber = _cal.firstDayNumber;
     moment.locale('sv');
     $.each(moment.weekdays(true), function (i, weekday) {
-        calendar += '<tr><td scope="rowgroup" class="weekday">' + weekday + '</td>';
+        calendar += '<tr><td class="weekday">' + weekday + '</td>';
 
         for (let colspanNumber = 0, l = _cal.numberOfWeeks; colspanNumber < l; colspanNumber++) {
             let day = dayNumber + i + (colspanNumber * 7);
@@ -158,6 +224,5 @@ function initCalendar() {
 
 
 $(document).ready(function () {
-
     initPage();
 });
---
+++ trains.js
@@ -0,0 +1,116 @@
+let trains = [
+    {
+        days: 'railbus',
+        type: 'railbus',
+        nr: 2, name: 'Kaffetåget',
+        reversed: false,
+        tdb: [{sign: 'fr N', time: '10:00'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '10:20'}, {sign: 'fr J', time: '10:25'},
+            {sign: 'Tp', time: '10:29'}, {sign: 't Lt', time: '10:38'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 3, name: 'Kaffetåget', reversed: true,
+        tdb: [{sign: 'fr N', time: '11:30'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '11:10'}, {sign: 'fr J', time: '11:00'},
+            {sign: 'Tp', time: '10:56'}, {sign: 't Lt', time: '10:47'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 21, name: 'Gruvtåget', reversed: false,
+        tdb: [{sign: 'fr N', time: '11:50'}, {sign: 'Phn', time: 'x'}, {sign: 'Ph', time: '|'},
+            {sign: 't Phö', time: '12:06'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 22, name: 'Gruvtåget', reversed: true,
+        tdb: [{sign: 'fr N', time: '12:36'}, {sign: 'Phn', time: 'x'}, {sign: 'Ph', time: '|'},
+            {sign: 't Phö', time: '12:20'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 23, name: 'Utflykttåget', reversed: false,
+        tdb: [{sign: 'fr N', time: '11:50'}, {sign: 'Phn', time: 'x'}, {sign: 'Ph', time: '|'},
+            {sign: 't Phö', time: '12:06'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 24, name: 'Utflykttåget', reversed: true,
+        tdb: [{sign: 'fr N', time: '15:40'}, {sign: 'Phn', time: 'x'}, {sign: 'Ph', time: '|'},
+            {sign: 't Phö', time: '15:24'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 4, name: 'Fikatåget', reversed: false,
+        tdb: [{sign: 'fr N', time: '13:20'}, {sign: 'Sm', time: 'x'}, {sign: 'Lm', time: '|'},
+            {sign: 't J', time: '13:40'}, {sign: 'fr J', time: '13:45'}, {sign: 'Tp', time: '13:49'},
+            {sign: 't Lt', time: '13:58'}]
+    }, {
+        days: 'railbus', type: 'railbus', nr: 5, name: 'Fikatåget', reversed: true,
+        tdb: [{sign: 'fr N', time: '14:45'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '14:25'}, {sign: 'fr J', time: '14:20'},
+            {sign: 'Tp', time: '14:16'}, {sign: 't Lt', time: '14:07'}]
+    }, {
+        days: 'steam', type: 'steam', nr: 202, name: 'Kaffetåget', reversed: false,
+        tdb: [{sign: 'fr N', time: '10:00'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '10:20'}]
+    }, {
+        days: 'steam', type: 'steam', nr: 203, name: 'Kaffetåget', reversed: true,
+        tdb: [{sign: 'fr N', time: '11:30'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '11:10'}]
+    }, {
+        days: 'steam', type: 'railbus', nr: 19, name: 'Badtåget', reversed: false,
+        tdb: [{sign: 'fr N', time: '10:10'}, {sign: 'Gt', time: '10:21'}, {sign: 'Kä', time: '10:22'},
+            {sign: 'Bt', time: 'x'}, {sign: 'Vk', time: '10:41'}, {sign: 'Nvk', time: '10:46'}]
+    }, {
+        days: 'steam', type: 'railbus', nr: 20, name: 'Badtåget', reversed: true,
+        tdb: [{sign: 'fr N', time: '11:30'}, {sign: 'Gt', time: '11:20'}, {sign: 'Kä', time: '11:19'},
+            {sign: 'Bt', time: 'x'}, {sign: 'Vk', time: '11:00'}, {sign: 'Nvk', time: '10:55'}]
+    }, {
+        days: 'steam',
+        type: 'railbus',
+        nr: 23, name: 'Gruvtåget',
+        reversed: false,
+        tdb: [{sign: 'fr N', time: '11:50'}, {sign: 'Phn', time: 'x'}, {sign: 'Ph', time: '|'},
+            {sign: 't Phö', time: '12:06'}, {sign: 'fr Phö', time: '12:15'}, {sign: 'Gt', time: '12:18'},
+            {sign: 'Kä',time: '12:19'}]
+    }, {
+        days: 'steam',
+        type: 'railbus',
+        nr: 24, name: 'Gruvtåget',
+        reversed: true,
+        tdb: [{sign: 'fr N', time: '12:50'}, {sign: 'Phn', time: 'x'}, {sign: 'Ph', time: '|'},
+            {sign: 'Gt', time: '12:46'}, {sign: 'Kä', time: '12:45'}]
+    }, {
+        days: 'steam',
+        type: 'steam',
+        nr: 204, name: 'Lunchtåget',
+        reversed: false,
+        tdb: [{sign: 'fr N', time: '12:00'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '12:20'}]
+    }, {
+        days: 'steam',
+        type: 'steam',
+        nr: 205, name: 'Lunchtåget',
+        reversed: true,
+        tdb: [{sign: 'fr N', time: '13:30'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '13:10'}]
+    }, {
+        days: 'steam',
+        type: 'railbus',
+        nr: 25, name: 'Utflykts-\ntåget',
+        reversed: false,
+        tdb: [{sign: 'fr N', time: '14:00'}, {sign: 'Gt', time: '14:11'}, {sign: 'Kä', time: 'x'},
+            {sign: 'Bt', time: 'x'}, {sign: 'Vk', time: '14:31'}, {sign: 'Nvk', time: '14:36'}]
+    }, {
+        days: 'steam',
+        type: 'railbus',
+        nr: 26, name: 'Utflykts-\ntåget',
+        reversed: true,
+        tdb: [{sign: 'fr N', time: '15:46'}, {sign: 'fr Phö', time: '15:30'}, {sign: 't Phö', time: '15:23'},
+            {sign: 'Gt', time: '15:20'}, {sign: 'Kä', time: '15:19'}, {sign: 'Bt', time: 'x'},
+            {sign: 'Vk', time: '14:50'}, {sign: 'Nvk', time: '14:45'}]
+    }, {
+        days: 'steam',
+        type: 'steam',
+        nr: 206, name: 'Eftermiddags\nångan',
+        reversed: false,
+        tdb: [{sign: 'fr N', time: '14:30'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '14:50'}]
+    }, {
+        days: 'steam',
+        type: 'steam',
+        nr: 207, name: 'Eftermiddags\nångan',
+        reversed: true,
+        tdb: [{sign: 'fr N', time: '16:00'}, {sign: 'Kh', time: 'x'}, {sign: 'Sm', time: 'x'},
+            {sign: 'Lm', time: 'x'}, {sign: 't J', time: '15:40'}]
+    },
+];
