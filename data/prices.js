e7044c6906a13f9bf7879369212092176e4f6fab
Magnus Lundgren <magnus@docilitas.se>
2022-03-22 16:26:21 +0100

    Prices adjusted.

--- data/prices.js
+++ data/prices.js
@@ -2,5 +2,5 @@ let prices = [
     'Nora - Järle (ToR), 150:-',
     'Nora - Pershytan/Gyttorp (ToR), 100:-',
     'Nora - Vikersvik (ToR), 150:-',
-    'Barn, 6-15 år, 25:-',
+    'Barn, 3-15 år, 25:- i målsmans sällskap',
 ];
