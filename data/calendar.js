171353f77e7932c7915fca3ffe64cb61d86237cc
Magnus Lundgren <magnus@docilitas.se>
2023-03-20 08:25:31 +0100

    Move data to the webshop

--- data/calendar.js
+++ data/calendar.js
@@ -1,7 +1,7 @@
 // This file is handled from ångtågetutinora.se
 
 // Set first and last week to be printed within the calendar
-let weeksToShow = [20, 36];
+let weeksToShow = [24, 36];
 
 // Set to empty to disable watermark
 let watermark = "";
